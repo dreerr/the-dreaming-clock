77af121177b368f1db8cb7bfef9be148ade0b5e4
Diktus Dreibholz <dreerr@gmail.com>
2018-08-29 08:36:51 +0200

    cosmetic changes

--- src/leds.h
+++ src/leds.h
@@ -23,6 +23,7 @@ CRGB not_set_color = CRGB(255, 0, 15);
 unsigned long last_millis = millis();
 
 void setDecimal(int position, int value, int amount) {
+  // Array for positions of segments
   bool seven_seg_digits[10][7] = {
       {1, 1, 1, 0, 1, 1, 1},  // = 0
       {0, 0, 1, 0, 0, 0, 1},  // = 1
@@ -35,22 +36,25 @@ void setDecimal(int position, int value, int amount) {
       {1, 1, 1, 1, 1, 1, 1},  // = 8
       {0, 1, 1, 1, 1, 1, 1}   // = 9
   };
+  // Set opacity of 7 segments starting in startposition
   int start = position * 7;
   for (byte segPos = 0; segPos < 7; segPos++) {
     if (seven_seg_digits[value][segPos]) {
       segs[start + segPos].opacity = amount;
     }
   }
-  }
+}
 
-  void setNumber(int value, int amount) {
-    const int numDigits = 4;
-    for (int i = 0; i < numDigits; i++) {
-      setDecimal(i, value / ((int)pow(10, numDigits - 1 - i)) % 10, amount);
-    }
+void setNumber(int value, int amount) {
+  // Convenience function to set a number
+  const int numDigits = 4;
+  for (int i = 0; i < numDigits; i++) {
+    setDecimal(i, value / ((int)pow(10, numDigits - 1 - i)) % 10, amount);
+  }
 }
 
 void randomness() {
+  // NOTE: Not in use anymore
   if(!awake && time_was_set) {
     for (int i = 0; i < 7 * 4; i++) {
       //segs[i].setRandom();
@@ -59,11 +63,13 @@ void randomness() {
 }
 
 void showCurrentTime() {
+  // First set all digits to zero opacity, then set the time
   setNumber(8888, 0);
   setNumber(minute() + hour() * 100, 255);
 }
 
 void goSleep() {
+  // Go sleep again, after end of timer
   awake = false;
   for (int i = 0; i < 7 * 4; i++) {
     segs[i].mode = SegmentMode::RANDOM;
@@ -89,7 +95,7 @@ void setupLEDs() {
 
 void loopLEDs() {
   // Only execute very x FPS
-  if((millis() - last_millis) < (1000/FRAMES_PER_SECOND)) return;
+  if((millis() - last_millis) < (1000 / FRAMES_PER_SECOND)) return;
   last_millis = millis();
 
 
